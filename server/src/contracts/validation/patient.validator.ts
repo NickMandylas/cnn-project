dc6561183dae28f937d27da72c539cff748f0825
Nick Mandylas <nick.mandylas@gmail.com>
2021-07-27 22:04:38 +1000

    added patient edit resolver

--- server/src/contracts/validation/patient.validator.ts
+++ server/src/contracts/validation/patient.validator.ts
@@ -1,10 +1,38 @@
-import { IsEmail, Length } from "class-validator";
+import { IsEmail, IsUUID, Length } from "class-validator";
 import { Field, InputType } from "type-graphql";
 
 type SexType = "Male" | "Female";
 
 @InputType()
 export class CreatePatientInput {
+  @Field()
+  @Length(1, 255, { message: "First name is too small." })
+  firstName: string;
+
+  @Field()
+  @Length(1, 255, { message: "Second name is too small" })
+  lastName: string;
+
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  dateOfBirth: string;
+
+  @Field()
+  sex: SexType;
+
+  @Field({ nullable: true })
+  notes: string;
+}
+
+@InputType()
+export class EditPatientInput {
+  @Field()
+  @IsUUID("4")
+  id: string;
+
   @Field()
   @Length(1, 255, { message: "First name is too small." })
   firstName: string;
