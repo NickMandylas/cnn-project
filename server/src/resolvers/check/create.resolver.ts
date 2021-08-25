4dd4e6bf9a867b0706a021cf21492d269ecd0a69
Nick Mandylas <nick.mandylas@gmail.com>
2021-08-25 15:22:40 +1000

    added create check resolver

---
+++ server/src/resolvers/check/create.resolver.ts
@@ -0,0 +1,69 @@
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { GraphQLUpload, FileUpload } from "graphql-upload";
+import axios from "axios";
+import { ServerContext } from "@server/contracts/interfaces/serverContext";
+import { v4 } from "uuid";
+import { Check } from "@server/entities/check.entity";
+import { CheckResponse } from "@server/shared/responses/check";
+
+const bucketName = "cnn-skin-lesion-images";
+
+type PredictionResults = {
+  confidence: string;
+  variant: string;
+};
+
+@Resolver()
+export class CreateCheckResolver {
+  @Mutation(() => CheckResponse)
+  async createCheck(
+    @Arg("localisation") localisation: string,
+    @Arg("patientId") patientId: string,
+    @Arg("file", () => GraphQLUpload)
+    { createReadStream, filename }: FileUpload,
+    @Ctx() { em, storage }: ServerContext,
+  ): Promise<CheckResponse> {
+    const uuid = v4();
+    const scanName = `checks/${uuid}_${filename}`;
+
+    console.log(localisation, patientId, uuid);
+
+    const prediction = new Promise((resolve, reject) =>
+      createReadStream()
+        .pipe(
+          storage.bucket(bucketName).file(scanName).createWriteStream({
+            resumable: false,
+            gzip: false,
+          }),
+        )
+        .on("error", (err) => {
+          reject(err);
+        })
+        .on("finish", async () => {
+          const res = await axios.post(
+            "https://australia-southeast1-cnn-project-321314.cloudfunctions.net/lesion-check-pub",
+            JSON.stringify({ model: "basic_model.h5", image: scanName }),
+            { headers: { "Content-Type": "application/json; charset=utf-8" } },
+          );
+
+          resolve(res.data);
+        }),
+    );
+
+    const results = (await prediction) as PredictionResults;
+
+    const check = em.create(Check, {
+      id: uuid,
+      confidence: results.confidence,
+      variant: results.variant,
+      localisation,
+      patient: patientId,
+      scan: scanName,
+      scanDate: new Date(),
+    });
+
+    await em.persistAndFlush(check);
+
+    return { check };
+  }
+}
